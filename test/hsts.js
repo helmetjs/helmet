2daec9ada7ddb871d951fe6836a670ee88cc91fd
Matt <matt@mattjay.com>
2013-12-06 15:59:31 -0600

    JSHint 4 indent instead of 2. fixed.

--- test/hsts.js
+++ test/hsts.js
@@ -4,31 +4,31 @@ var connect = require('connect');
 var request = require('supertest');
 
 describe('hsts', function () {
-  "use strict";
+    "use strict";
 
-  var app;
-  beforeEach(function () {
-    app = connect();
-  });
-
-  it('sets header properly with no args', function (done) {
-    app.use(helmet.hsts());
-    app.use(function (req, res) {
-      res.end('Hello world!');
+    var app;
+    beforeEach(function () {
+        app = connect();
     });
-    request(app).get('/')
-      .set('x-forwarded-proto', 'https')
-      .expect('Strict-Transport-Security', 'max-age=15768000', done);
-  });
 
-  it('sets header properly with args', function (done) {
-    app.use(helmet.hsts('1234', true));
-    app.use(function (req, res) {
-      res.end('Hello world!');
+    it('sets header properly with no args', function (done) {
+        app.use(helmet.hsts());
+        app.use(function (req, res) {
+            res.end('Hello world!');
+        });
+        request(app).get('/')
+        .set('x-forwarded-proto', 'https')
+        .expect('Strict-Transport-Security', 'max-age=15768000', done);
     });
-    request(app).get('/')
-      .set('x-forwarded-proto', 'https')
-      .expect('Strict-Transport-Security', 'max-age=1234; includeSubdomains', done);
-  });
+
+    it('sets header properly with args', function (done) {
+        app.use(helmet.hsts('1234', true));
+        app.use(function (req, res) {
+            res.end('Hello world!');
+        });
+        request(app).get('/')
+        .set('x-forwarded-proto', 'https')
+        .expect('Strict-Transport-Security', 'max-age=1234; includeSubdomains', done);
+    });
 
 });
