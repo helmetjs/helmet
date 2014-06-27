8fe298b6b95459b3b44d4535f4aca1ca76b9d757
Evan Hahn <me@evanhahn.com>
2014-06-26 23:54:30 -0700

    Add error about deprecated `helmet.defaults`

--- test/deprecated.js
+++ test/deprecated.js
@@ -4,13 +4,18 @@ var assert = require('assert');
 
 describe('deprecated methods', function () {
 
-    var deprecatedList = ['iexss', 'contentTypeOptions', 'cacheControl'];
+    var deprecatedList = [
+        'iexss',
+        'contentTypeOptions',
+        'cacheControl',
+        'defaults'
+    ];
 
     deprecatedList.forEach(function (deprecated) {
         it(deprecated + ' is deprecated', function() {
             assert.throws(function () {
                 helmet[deprecated]();
-            });
+            }, /deprecated/i);
         });
     });
 
