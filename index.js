264f661f4215c30179ddce3ed69be13b2f8efeba
Geert Josten <geert.josten@marklogic.com>
2016-02-28 20:36:55 +0100

    Got rid of jshint comments, extra if around overrides, and simplified alias to module lookup

--- index.js
+++ index.js
@@ -1,50 +1,43 @@
-/* jshint node: true */
-/* jshint asi: true */
 var connect = require('connect')
 
 var config = require('./config.json')
 
-function findModuleName(alias) {
-  'use strict';
-  var result = null;
-  Object.keys(config.middlewares).forEach(function (moduleName) {
-    if (alias === moduleName || config.middlewares[moduleName].indexOf(alias) > -1) {
-      result = moduleName
-    }
-  })
-  return result
-}
-
 function helmet (optionOverrides) {
-  'use strict';
+  'use strict'
   var chain = connect()
+
+  // reverse lookup for alias to moduleName
+  var aliases = {}
+  Object.keys(config.middlewares).forEach(function (moduleName) {
+    aliases[moduleName] = moduleName
+    config.middlewares[moduleName].forEach(function (alias) {
+      aliases[alias] = moduleName
+    })
+  })
 
   // init options for default middleware
   var options = {}
   config.defaultMiddleware.forEach(function (middlewareName) {
-    var moduleName = findModuleName(middlewareName)
-    // TODO: initialize with real default props from module?
+    var moduleName = aliases[middlewareName]
     options[moduleName] = {}
   })
 
   // override options with settings provided externally
-  Object.keys(optionOverrides).forEach(function (middlewareName) {
-    var overrides = optionOverrides[middlewareName]
-    var moduleName = findModuleName(middlewareName)
-    // console.log(middlewareName)
-    // console.log(moduleName)
-    // console.log(overrides)
-    if (typeof overrides === 'boolean') {
-      if (overrides) {
-        // TODO: initialize with real default props from module?
-        options[moduleName] = {}
+  if (optionOverrides) {
+    Object.keys(optionOverrides).forEach(function (middlewareName) {
+      var overrides = optionOverrides[middlewareName]
+      var moduleName = aliases[middlewareName]
+      if (typeof overrides === 'boolean') {
+        if (overrides) {
+          options[moduleName] = {}
+        } else {
+          delete options[moduleName]
+        }
       } else {
-        delete options[moduleName]
+        options[moduleName] = overrides
       }
-    } else {
-      options[moduleName] = overrides
-    }
-  })
+    })
+  }
 
   // build chain of helmet modules
   Object.keys(options).forEach(function (moduleName) {
@@ -53,12 +46,12 @@ function helmet (optionOverrides) {
       Object.keys(options[moduleName]).length ? options[moduleName] : undefined
     ))
   })
-  
+
   return chain
 }
 
 Object.keys(config.middlewares).forEach(function (moduleName) {
-  'use strict';
+  'use strict'
   var pkg = require(moduleName)
   var aliases = config.middlewares[moduleName]
   helmet[moduleName] = pkg
