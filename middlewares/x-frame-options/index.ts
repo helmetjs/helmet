6fd0fbdc6d4a9a18a3b88886918f149a47102091
Evan Hahn <me@evanhahn.com>
2020-12-27 14:33:05 -0600

    X-Frame-Options: use `switch` for clarity
    I think this is cleaner with `switch`, and also saves a few bytes.

--- middlewares/x-frame-options/index.ts
+++ middlewares/x-frame-options/index.ts
@@ -10,18 +10,20 @@ function getHeaderValueFromOptions({
   const normalizedAction =
     typeof action === "string" ? action.toUpperCase() : action;
 
-  if (normalizedAction === "SAME-ORIGIN") {
-    return "SAMEORIGIN";
-  } else if (normalizedAction === "DENY" || normalizedAction === "SAMEORIGIN") {
-    return normalizedAction;
-  } else if (normalizedAction === "ALLOW-FROM") {
-    throw new Error(
-      "X-Frame-Options no longer supports `ALLOW-FROM` due to poor browser support. See <https://github.com/helmetjs/helmet/wiki/How-to-use-X%E2%80%93Frame%E2%80%93Options's-%60ALLOW%E2%80%93FROM%60-directive> for more info."
-    );
-  } else {
-    throw new Error(
-      `X-Frame-Options received an invalid action ${JSON.stringify(action)}`
-    );
+  switch (normalizedAction) {
+    case "SAME-ORIGIN":
+      return "SAMEORIGIN";
+    case "DENY":
+    case "SAMEORIGIN":
+      return normalizedAction;
+    case "ALLOW-FROM":
+      throw new Error(
+        "X-Frame-Options no longer supports `ALLOW-FROM` due to poor browser support. See <https://github.com/helmetjs/helmet/wiki/How-to-use-X%E2%80%93Frame%E2%80%93Options's-%60ALLOW%E2%80%93FROM%60-directive> for more info."
+      );
+    default:
+      throw new Error(
+        `X-Frame-Options received an invalid action ${JSON.stringify(action)}`
+      );
   }
 }
 
