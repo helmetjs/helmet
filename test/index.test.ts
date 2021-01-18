0512673017cf3f7af30963442f5c5ff1a9293717
Evan Hahn <me@evanhahn.com>
2021-01-18 11:54:29 -0600

    Test that Origin-Agent-Cluster is missing from default

--- test/index.test.ts
+++ test/index.test.ts
@@ -27,6 +27,7 @@ describe("helmet", () => {
       "content-security-policy":
         "default-src 'self';base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
       "expect-ct": "max-age=0",
+      "origin-agent-cluster": null,
       "referrer-policy": "no-referrer",
       "strict-transport-security": "max-age=15552000; includeSubDomains",
       "x-content-type-options": "nosniff",
