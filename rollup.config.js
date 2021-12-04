dbd2566875147aa4e4aeb4f69e7c457362aa3dbc
Evan Hahn <me@evanhahn.com>
2021-12-04 16:56:16 -0600

    Building middleware packages (but maybe not quite right)

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,16 @@
+import * as path from "path";
+import * as fs from "fs";
+import { fileURLToPath } from "url";
 import typescript from "@rollup/plugin-typescript";
 
 const defaults = {
   input: "index.ts",
   plugins: [typescript()],
 };
+
+const thisPath = fileURLToPath(import.meta.url);
+const middlewaresPath = path.join(path.dirname(thisPath), "middlewares");
+const middlewares = fs.readdirSync(middlewaresPath);
 
 export default [
   {
@@ -20,4 +27,13 @@ export default [
       file: "dist/index.js",
     },
   },
+  ...middlewares.map((middleware) => ({
+    ...defaults,
+    input: path.join(middlewaresPath, middleware, "index.ts"),
+    output: {
+      exports: "named",
+      dir: `dist/middlewares/${middleware}`,
+      format: "cjs",
+    },
+  })),
 ];
