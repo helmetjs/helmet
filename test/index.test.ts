3874c6b66d634bf46680c6e2fc20fa7bd4629f9b
fcrozatier <frederic.crozatier@protonmail.com>
2022-08-03 09:55:10 -0500

    Content-Security-Policy: remove `block-all-mixed-content`
    To quote [MDN][mdn]:
    
    > Deprecated: This feature is no longer recommended. Though some
    > browsers might still support it, it may have already been removed from
    > relevant web standards [...]. Avoid using it, and update existing code
    > if possible [...]
    
    See [#371][371] and [#372][372].
    
    [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/block-all-mixed-content
    [371]: https://github.com/helmetjs/helmet/pull/371
    [372]: https://github.com/helmetjs/helmet/pull/372

--- test/index.test.ts
+++ test/index.test.ts
@@ -30,7 +30,7 @@ describe("helmet", () => {
     // we should update this test to be more robust.
     const expectedHeaders = {
       "content-security-policy":
-        "default-src 'self';base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
+        "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
       "cross-origin-embedder-policy": "require-corp",
       "cross-origin-opener-policy": "same-origin",
       "cross-origin-resource-policy": "same-origin",
